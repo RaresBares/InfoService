5c7e75f94d01cc2ec4ae3f836dbf72b5347c7162
Rares <rsahleanu@gmail.com>
2020-02-09 12:39:28 +0100

    Init

--- src/de/service/api/Service.java
+++ src/de/service/api/Service.java
@@ -1,8 +1,15 @@
 package de.service.api;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public abstract class Service extends Thread {
+        private final ServerSocket serverSocket;
+    public Service(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
 
     public Service listen(){
         this.start();
@@ -12,6 +19,17 @@ public abstract class Service extends Thread {
 
     @Override
     public void run() {
-        super.run();
+       while (true){
+           try {
+               Socket t = serverSocket.accept();
+               if(t.getInetAddress().getHostAddress() == "127.0.0.1"){
+                        onConnect(t);
+               }else{
+                   System.err.println("External Connection IP: " + t.getInetAddress().getHostAddress());
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+       }
     }
 }
